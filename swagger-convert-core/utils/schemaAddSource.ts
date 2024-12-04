7b6150f693ad4b3afe6cc7e5d1fad0085683bba1
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-04 14:11:32 +0800

    Update schemaAddSource.ts

--- swagger-convert-core/utils/schemaAddSource.ts
+++ swagger-convert-core/utils/schemaAddSource.ts
@@ -1,7 +1,11 @@
-import { FetchingJSONSchemaStore, InputData, JSONSchemaInput } from "quicktype-core"
+import {
+  FetchingJSONSchemaStore,
+  InputData,
+  JSONSchemaInput,
+} from 'quicktype-core';
 
 interface SchemaAddSourceParams {
-  schemaJson: Record<string, any>;
+  schemaJson: Record<string, any> | string;
   /** 生成ts的名称类型 */
   name: string;
 }
@@ -9,15 +13,16 @@ export const schemaAddSource = async ({
   schemaJson,
   name,
 }: SchemaAddSourceParams) => {
-   // 创建一个 JSON 输入对象
-   const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore())
-   // 添加 JSON Schema 作为输入源
-   await schemaInput.addSource({
-     name: "GeneratedType",  // 生成类型的名称
-     schema: JSON.stringify(schemaJson)    // JSON Schema 字符串
-   })
- 
-   const inputData = new InputData()
-   inputData.addInput(schemaInput)
-   return inputData
+  // 创建一个 JSON 输入对象
+  const schemaInput = new JSONSchemaInput(new FetchingJSONSchemaStore());
+  // 添加 JSON Schema 作为输入源
+  await schemaInput.addSource({
+    name, // 生成类型的名称
+    schema:
+      typeof schemaJson === 'string' ? schemaJson : JSON.stringify(schemaJson), // JSON Schema 字符串
+  });
+
+  const inputData = new InputData();
+  inputData.addInput(schemaInput);
+  return inputData;
 };
