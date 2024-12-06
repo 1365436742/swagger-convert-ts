b00e0d9acbf16b9807af2728f233a0ad9faa8964
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-06 17:10:54 +0800

    Update index.ts

--- packages/swagger-convert-core/src/index.ts
+++ packages/swagger-convert-core/src/index.ts
@@ -1,26 +1,43 @@
 import fs from 'fs';
 import path from 'path';
 import { schemaAddSource } from './utils/schemaAddSource';
-import { generatedCode } from './utils/generatedCode';
+import {
+  generatedRequestCode,
+  generatedTsTypeCode,
+} from './utils/generatedCode';
 import { SwaggerOpenApiType } from './types';
 import { InputData } from 'quicktype-core';
 import { swaggerJsonToJsonSchema } from './utils/swaggerJsonToJsonSchema';
+import { copyFolderSync } from './utils';
 const swaggerOpenApi = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '../test/swagger/index.json'), 'utf8')
 ) as SwaggerOpenApiType;
-
 /**
  * https://github.com/glideapps/quicktype
  */
 const main = async () => {
+  console.log(swaggerOpenApi)
   const inputData = new InputData();
-  const jsonSchema = swaggerJsonToJsonSchema(swaggerOpenApi);
+  const { definitionSchemaJson, requestFileCodeSort } =
+    swaggerJsonToJsonSchema(swaggerOpenApi);
   const schemaInput = await schemaAddSource({
-    schemas: jsonSchema,
+    schemas: definitionSchemaJson,
   });
   inputData.addInput(schemaInput);
-
-  fs.writeFileSync('./test/output.json', JSON.stringify(jsonSchema, null, 2));
+  copyFolderSync('./template', './test');
+  // fs.unlinkSync('./test/request/request-test');
+  await generatedRequestCode(
+    requestFileCodeSort,
+    './test/request/request-test'
+  );
+  fs.writeFileSync(
+    './test/output.json',
+    JSON.stringify(definitionSchemaJson, null, 2)
+  );
+  fs.writeFileSync(
+    './test/functionCodeList.json',
+    JSON.stringify(requestFileCodeSort, null, 2)
+  );
 
   const code = await generatedTsTypeCode(inputData);
 
