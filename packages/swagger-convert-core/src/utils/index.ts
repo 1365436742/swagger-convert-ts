834e52e6a71a8942b5313487030b3b734642dbcd
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-06 17:10:20 +0800

    Update index.ts

--- packages/swagger-convert-core/src/utils/index.ts
+++ packages/swagger-convert-core/src/utils/index.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 export function capitalizeFirstLetter(str: string) {
   if (!str) return ''; // 处理空字符串或非字符串输入
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -16,7 +18,9 @@ export function typeNameGenerated(
         return capitalizeFirstLetter(str);
       })
       ?.join('') || '';
-  return tagsStr + capitalizeFirstLetter(operationId) + capitalizeFirstLetter(ext);
+  return (
+    tagsStr + capitalizeFirstLetter(operationId) + capitalizeFirstLetter(ext)
+  );
 }
 
 export function replaceRef($ref: string) {
@@ -41,4 +45,34 @@ export function replaceAllRefs(schema: Record<string, any>) {
     }
   }
   findRefs(schema);
+}
+/** 去掉下划线首字母大写 */
+export function toCamelCase(str: string) {
+  // 保留开头的下划线（如果有的话）
+  return str.replace(/^_?(.)|_(\d|\D)/g, function (_, firstLetter, letter) {
+    if (firstLetter) return firstLetter;
+    return letter.toUpperCase();
+  });
+}
+
+export function copyFolderSync(source: string, target: string) {
+  // 检查目标目录是否存在，不存在则创建
+  if (!fs.existsSync(target)) {
+    fs.mkdirSync(target, { recursive: true });
+  }
+  // 读取源目录中的所有文件/目录
+  const files: string[] = fs.readdirSync(source);
+  files.forEach((file) => {
+    const sourcePath = path.join(source, file);
+    const targetPath = path.join(target, file);
+
+    // 判断是文件还是目录
+    if (fs.lstatSync(sourcePath).isDirectory()) {
+      // 如果是目录，递归调用
+      copyFolderSync(sourcePath, targetPath);
+    } else {
+      // 如果是文件，直接复制
+      fs.copyFileSync(sourcePath, targetPath);
+    }
+  });
 }
