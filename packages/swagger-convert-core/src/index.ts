9b88cac17a3b96a185299e9c5bf5949d42743d7e
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-06 15:06:36 +0800

    Update index.ts

--- packages/swagger-convert-core/src/index.ts
+++ packages/swagger-convert-core/src/index.ts
@@ -1,43 +1,29 @@
 import fs from 'fs';
+import path from 'path';
 import { schemaAddSource } from './utils/schemaAddSource';
 import { generatedCode } from './utils/generatedCode';
+import { SwaggerOpenApiType } from './types';
 import { InputData } from 'quicktype-core';
+import { swaggerJsonToJsonSchema } from './utils/swaggerJsonToJsonSchema';
+const swaggerOpenApi = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../test/swagger/index.json'), 'utf8')
+) as SwaggerOpenApiType;
+
 /**
  * https://github.com/glideapps/quicktype
  */
 const main = async () => {
   const inputData = new InputData();
+  const jsonSchema = swaggerJsonToJsonSchema(swaggerOpenApi);
   const schemaInput = await schemaAddSource({
-    name: 'GenerAA',
-    schemaJson: {
-      type: 'object',
-      properties: {
-        union: {
-          oneOf: [
-            {
-              type: 'boolean',
-            },
-            {
-              type: 'number',
-            },
-          ],
-        },
-        enum: {
-          type: 'string',
-          enum: ['red', 'green', 'blue'],
-        },
-        foo: {
-          type: 'string',
-        },
-        bar: {
-          type: 'string',
-        },
-      },
-      additionalProperties: false,
-    },
+    schemas: jsonSchema,
   });
   inputData.addInput(schemaInput);
+
+  fs.writeFileSync('./test/output.json', JSON.stringify(jsonSchema, null, 2));
+
   const code = await generatedCode(inputData);
+
   fs.writeFileSync('./test/type.d.ts', code);
 };
 main();
