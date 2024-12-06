f4391ba4926688c7b6f0d97482315728f5023152
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-06 18:13:36 +0800

    Update index.ts

--- packages/swagger-convert-core/src/index.ts
+++ packages/swagger-convert-core/src/index.ts
@@ -9,8 +9,17 @@ import { SwaggerOpenApiType } from './types';
 import { InputData } from 'quicktype-core';
 import { swaggerJsonToJsonSchema } from './utils/swaggerJsonToJsonSchema';
 import { copyFolderSync } from './utils';
+
+const config = {
+  /** swagger地址。支持https|http、本地文件目录 */
+  swaggerOpenApiUrl: path.resolve(__dirname, '../test/swagger/test2.json'),
+  generatedCodeFileUrl: './test',
+  /** 生成本次接口的应用空间名称 */
+  requestSpanceName: 'request-test',
+};
+const { generatedCodeFileUrl, swaggerOpenApiUrl, requestSpanceName } = config;
 const swaggerOpenApi = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../test/swagger/test2.json'), 'utf8')
+  fs.readFileSync(swaggerOpenApiUrl, 'utf8')
 ) as SwaggerOpenApiType;
 /**
  * https://github.com/glideapps/quicktype
@@ -23,11 +32,14 @@ const main = async () => {
     schemas: definitionSchemaJson,
   });
   inputData.addInput(schemaInput);
-  copyFolderSync(path.join(__dirname, './template'), './test');
+  copyFolderSync(path.join(__dirname, './template'), generatedCodeFileUrl);
   await generatedRequestCode(
     requestFileCodeSort,
-    './test/request/request-test'
+    path.join(config.generatedCodeFileUrl, 'request', requestSpanceName)
   );
+
+  const code = await generatedTsTypeCode(inputData);
+
   fs.writeFileSync(
     './test/output.json',
     JSON.stringify(definitionSchemaJson, null, 2)
@@ -36,9 +48,6 @@ const main = async () => {
     './test/functionCodeList.json',
     JSON.stringify(requestFileCodeSort, null, 2)
   );
-
-  const code = await generatedTsTypeCode(inputData);
-
   fs.writeFileSync('./test/type.d.ts', code);
 };
 main();
