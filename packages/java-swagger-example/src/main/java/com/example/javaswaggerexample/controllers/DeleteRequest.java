471017252717084cb91235fbcbb9c1fd3bd06098
热情仔 <1365436742@qq.com>
2024-12-07 23:34:57 +0800

    feat: 添加基本json内容

---
+++ packages/java-swagger-example/src/main/java/com/example/javaswaggerexample/controllers/DeleteRequest.java
@@ -0,0 +1,39 @@
+package com.example.javaswaggerexample.controllers;
+
+import com.example.javaswaggerexample.model.Item;
+import com.example.javaswaggerexample.model.ItemBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/getRequest")
+public class PutRequest {
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable Long id) {
+       return id + "";
+    }
+    @GetMapping("/users/{id}/{spanceId}")
+    public String getUserByIds(@PathVariable Long id,@PathVariable String spanceId) {
+        return id + spanceId + "";
+    }
+    @GetMapping("/search")
+    public ArrayList<Item> searchItems(@RequestParam int length,@RequestParam int query, @RequestParam String text) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < length; i++) {
+            list.add(new Item(query, text));
+        }
+        // 处理逻辑
+        return list;
+    }
+    @GetMapping("/getBody")
+    public ArrayList<ItemBody> bodyListItems(@RequestBody ItemBody itemBody,@RequestParam int length) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < length; i++) {
+            list.add(itemBody);
+        }
+        // 处理逻辑
+        return list;
+    }
+}
+
--- packages/swagger-convert-core/test/type.d.ts
+++ packages/swagger-convert-core/test/type.d.ts
@@ -1,7 +1,37 @@
 namespace IApi {
+  type DeleteRequestGetUserByIdUsingDELETEResponses = string;
+
+  type DeleteRequestGetUserByIdsUsingDELETEResponses = string;
+
   type GetRequestGetUserByIdUsingGETResponses = string;
 
+  type PutRequestGetUserByIdUsingPUTResponses = string;
+
+  type PostRequestGetUserByIdUsingPOSTResponses = string;
+
+  type PatchRequestGetUserByIdUsingPATCHResponses = string;
+
   type GetRequestGetUserByIdsUsingGETResponses = string;
+
+  type PutRequestGetUserByIdsUsingPUTResponses = string;
+
+  type PostRequestGetUserByIdsUsingPOSTResponses = string;
+
+  type PatchRequestGetUserByIdsUsingPATCHResponses = string;
+
+  export type DeleteItemBody = {
+    /**
+     * 删除列表内容
+     */
+    listItems?: DeleteItemBodyListItem[];
+    name?: string;
+    text?: string;
+  };
+
+  export type DeleteItemBodyListItem = {
+    number?: number;
+    text?: string;
+  };
 
   export type Item = {
     number?: number;
@@ -9,7 +39,7 @@ namespace IApi {
   };
 
   export type ItemBody = {
-    listItems?: ListItemElement[];
+    listItems?: ItemBodyListItem[];
     /**
      * 名称
      */
@@ -20,9 +50,51 @@ namespace IApi {
     text?: string;
   };
 
-  export type ListItemElement = {
+  export type ItemBodyListItem = {
     number?: number;
     text?: string;
+  };
+
+  export type DeleteRequestBodyListItemsUsingDELETEParams = {
+    length?: number;
+  };
+
+  export type DeleteRequestBodyListItemsUsingDELETEBody = {
+    /**
+     * 删除列表内容
+     */
+    listItems?: DeleteItemBodyListItem[];
+    name?: string;
+    text?: string;
+  };
+
+  export type DeleteRequestBodyListItemsUsingDELETEResponses = {
+    /**
+     * 删除列表内容
+     */
+    listItems?: DeleteItemBodyListItem[];
+    name?: string;
+    text?: string;
+  };
+
+  export type DeleteRequestSearchItemsUsingDELETEParams = {
+    length?: number;
+    query?: number;
+    text?: string;
+  };
+
+  export type DeleteRequestSearchItemsUsingDELETEResponses = {
+    number?: number;
+    text?: string;
+  };
+
+  export type DeleteRequestGetUserByIdUsingDELETEParams = {
+    id?: number;
+  };
+
+  export type DeleteRequestGetUserByIdsUsingDELETEParams = {
+    id?: number;
+    spanceId?: string;
   };
 
   export type GetRequestBodyListItemsUsingGETParams = {
@@ -30,7 +102,7 @@ namespace IApi {
   };
 
   export type GetRequestBodyListItemsUsingGETBody = {
-    listItems?: ListItemElement[];
+    listItems?: ItemBodyListItem[];
     /**
      * 名称
      */
@@ -42,7 +114,91 @@ namespace IApi {
   };
 
   export type GetRequestBodyListItemsUsingGETResponses = {
-    listItems?: ListItemElement[];
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PutRequestBodyListItemsUsingPUTParams = {
+    length?: number;
+  };
+
+  export type PutRequestBodyListItemsUsingPUTBody = {
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PutRequestBodyListItemsUsingPUTResponses = {
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PostRequestBodyListItemsUsingPOSTParams = {
+    length?: number;
+  };
+
+  export type PostRequestBodyListItemsUsingPOSTBody = {
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PostRequestBodyListItemsUsingPOSTResponses = {
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PatchRequestBodyListItemsUsingPATCHParams = {
+    length?: number;
+  };
+
+  export type PatchRequestBodyListItemsUsingPATCHBody = {
+    listItems?: ItemBodyListItem[];
+    /**
+     * 名称
+     */
+    name?: string;
+    /**
+     * 描述
+     */
+    text?: string;
+  };
+
+  export type PatchRequestBodyListItemsUsingPATCHResponses = {
+    listItems?: ItemBodyListItem[];
     /**
      * 名称
      */
@@ -64,11 +220,71 @@ namespace IApi {
     text?: string;
   };
 
+  export type PutRequestSearchItemsUsingPUTParams = {
+    length?: number;
+    query?: number;
+    text?: string;
+  };
+
+  export type PutRequestSearchItemsUsingPUTResponses = {
+    number?: number;
+    text?: string;
+  };
+
+  export type PostRequestSearchItemsUsingPOSTParams = {
+    length?: number;
+    query?: number;
+    text?: string;
+  };
+
+  export type PostRequestSearchItemsUsingPOSTResponses = {
+    number?: number;
+    text?: string;
+  };
+
+  export type PatchRequestSearchItemsUsingPATCHParams = {
+    length?: number;
+    query?: number;
+    text?: string;
+  };
+
+  export type PatchRequestSearchItemsUsingPATCHResponses = {
+    number?: number;
+    text?: string;
+  };
+
   export type GetRequestGetUserByIdUsingGETParams = {
     id?: number;
   };
 
+  export type PutRequestGetUserByIdUsingPUTParams = {
+    id?: number;
+  };
+
+  export type PostRequestGetUserByIdUsingPOSTParams = {
+    id?: number;
+  };
+
+  export type PatchRequestGetUserByIdUsingPATCHParams = {
+    id?: number;
+  };
+
   export type GetRequestGetUserByIdsUsingGETParams = {
+    id?: number;
+    spanceId?: string;
+  };
+
+  export type PutRequestGetUserByIdsUsingPUTParams = {
+    id?: number;
+    spanceId?: string;
+  };
+
+  export type PostRequestGetUserByIdsUsingPOSTParams = {
+    id?: number;
+    spanceId?: string;
+  };
+
+  export type PatchRequestGetUserByIdsUsingPATCHParams = {
     id?: number;
     spanceId?: string;
   };
