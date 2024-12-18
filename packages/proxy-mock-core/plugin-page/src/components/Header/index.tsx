c8a24c8dfd26e6e229bd1fd6aba17f7cd9b55a94
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-18 18:31:07 +0800

    Create index.tsx

---
+++ packages/proxy-mock-core/plugin-page/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.less';
+const Header: React.FC<{ children?: React.ReactNode }> = (props) => {
+  return <div className="header">{props.children}</div>;
+};
+
+export default Header;
