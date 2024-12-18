d62b798f82d40e7260d51d300096065f75cd98c0
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-18 18:30:40 +0800

    Create index.tsx

---
+++ packages/proxy-mock-core/plugin-page/src/components/FormItemMoncaoEditor/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import MonacoEditor, { EditorProps } from '@monaco-editor/react';
+import './index.less';
+interface FormItemMoncaoEditorProps {
+  value?: string;
+  onChange?: (content?: string) => void;
+  editorProps?: EditorProps;
+}
+const FormItemMoncaoEditor: React.FC<FormItemMoncaoEditorProps> = (props) => {
+  const { value, onChange, editorProps = {} } = props;
+  return (
+    <div className="custom-monaco-editor">
+      <MonacoEditor
+        height="calc(100vh - 315px)"
+        className="custom-monaco-editor"
+        defaultLanguage="javascript"
+        theme="vs-dark"
+        options={{
+          selectOnLineNumbers: true,
+          roundedSelection: false,
+          readOnly: false,
+          cursorStyle: 'line',
+          automaticLayout: true,
+        }}
+        {...editorProps}
+        defaultValue={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default FormItemMoncaoEditor;
