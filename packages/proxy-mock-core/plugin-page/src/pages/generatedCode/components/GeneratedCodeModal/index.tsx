4faea0f152c6e446c862666aebd534807c2a91c2
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:55:13 +0800

    Add files via upload

---
+++ packages/proxy-mock-core/plugin-page/src/pages/generatedCode/components/GeneratedCodeModal/index.tsx
@@ -0,0 +1,97 @@
+import { Button, Flex, Form, Modal, Popover } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
+import React from 'react';
+import ParseSwagger from './ParseSwagger';
+import './index.less';
+import FormItemMoncaoEditor from '../../../../components/FormItemMoncaoEditor';
+interface GeneratedCodeModalProps {
+  open?: boolean;
+  onChange?: (open: boolean) => void;
+}
+const defaultJson = ['/api/aaa', '/generatedCode/*'];
+const GeneratedCodeModal: React.FC<GeneratedCodeModalProps> = ({
+  open,
+  onChange,
+}) => {
+  return (
+    <Modal
+      width={900}
+      className="generated-code"
+      title={
+        <Flex gap="small">
+          <div>生成axios代码</div>
+          <Popover content="生成代码的地址是由插件填写路径配置的">
+            <QuestionCircleOutlined />
+          </Popover>
+        </Flex>
+      }
+      open={open}
+      onCancel={() => onChange?.(false)}
+      footer={null}
+    >
+      <Form
+        name="GeneratedCodeModal"
+        labelAlign="left"
+        layout="vertical"
+        autoComplete="off"
+        initialValues={{ urlFilter: JSON.stringify(defaultJson, null, 2) }}
+        style={{ paddingTop: '10px' }}
+      >
+        <Form.Item
+          name="swaggerUrl"
+          label={
+            <Flex gap="small">
+              <div>swaggerUrl</div>
+              <Popover
+                content={
+                  <>
+                    可以参考：
+                    <a
+                      href="https://github.com/1365436742/swagger-convert-ts/blob/main/packages/java-swagger-example/pom.xml"
+                      target="__blank"
+                    >
+                      java的swagger示例
+                    </a>
+                    <br />
+                    输入本地或者远程的：http://localhost:8081/v3/api-docs
+                  </>
+                }
+              >
+                <QuestionCircleOutlined />
+              </Popover>
+            </Flex>
+          }
+        >
+          <ParseSwagger />
+        </Form.Item>
+        <Form.Item
+          name="urlFilter"
+          label={
+            <Flex gap="small">
+              <div>指定接口过滤（不填默认生成全部接口）</div>
+              <Popover content="生成后会覆盖所有空间下的文件。请选择所有你需要用到的接口">
+                <QuestionCircleOutlined />
+              </Popover>
+            </Flex>
+          }
+        >
+          <FormItemMoncaoEditor
+            editorProps={{
+              defaultLanguage: 'json',
+            }}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Flex gap="small" justify="flex-end">
+            <Button type="primary" htmlType="submit">
+              生成代码
+            </Button>
+            <Button onClick={() => onChange?.(false)}>取消</Button>
+          </Flex>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default GeneratedCodeModal;
