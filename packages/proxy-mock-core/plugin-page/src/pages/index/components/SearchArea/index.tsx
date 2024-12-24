76a8c01d4efcf80f0b3b9e90ed1289d20f995945
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:24:22 +0800

    Update index.tsx

--- packages/proxy-mock-core/plugin-page/src/pages/index/components/SearchArea/index.tsx
+++ packages/proxy-mock-core/plugin-page/src/pages/index/components/SearchArea/index.tsx
@@ -2,28 +2,28 @@ import { Button, Flex, Input } from 'antd';
 import './index.less';
 import UpdataMockModal from '../UpdataMockModal';
 import { useState } from 'react';
-import GeneratedCodeModal from '../GeneratedCodeModal';
+import GeneratedMockModal from '../GeneratedMockModal'
 const { Search } = Input;
 const SearchArea = () => {
   const [opneUpdataMockModal, setOpneUpdataMockModal] = useState(false);
-  const [opneGeneratedCodeModal, setGeneratedCodeModal] = useState(false);
+  const [opneGeneratedMockModal, setGeneratedMockModal] = useState(false);
   return (
     <div className="search-area">
       <UpdataMockModal
         open={opneUpdataMockModal}
         onChange={setOpneUpdataMockModal}
       ></UpdataMockModal>
-      <GeneratedCodeModal
-        open={opneGeneratedCodeModal}
-        onChange={setGeneratedCodeModal}
-      ></GeneratedCodeModal>
+      <GeneratedMockModal
+        open={opneGeneratedMockModal}
+        onChange={setGeneratedMockModal}
+      ></GeneratedMockModal>
       <Flex gap="small">
         <Search placeholder="搜索接口名称" allowClear />
         <Button type="primary" onClick={() => setOpneUpdataMockModal(true)}>
           添加mock数据
         </Button>
-        <Button type="primary" onClick={() => setGeneratedCodeModal(true)}>
-          生成axios请求代码
+        <Button type="primary" onClick={() => setGeneratedMockModal(true)}>
+          生成mockjs数据
         </Button>
       </Flex>
     </div>
