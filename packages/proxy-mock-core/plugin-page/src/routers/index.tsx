272cb4d28e71aa5f18932d79bb4c4296adef8ff8
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:23:21 +0800

    Update index.tsx

--- packages/proxy-mock-core/plugin-page/src/routers/index.tsx
+++ packages/proxy-mock-core/plugin-page/src/routers/index.tsx
@@ -1,24 +1,101 @@
-import { BrowserRouter, Route, RouteProps, Routes } from 'react-router-dom';
+import {
+  Route,
+  RouteProps,
+  Routes,
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+import { Button, Layout, Menu, MenuProps, theme } from 'antd';
+import { useState } from 'react';
+import {
+  AuditOutlined,
+  CodeOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons';
+import cls from 'classnames';
 import Index from '../pages/index';
+import GeneratedCode from '../pages/generatedCode';
 
-const Routers: RouteProps[] = [
+const { Content, Sider } = Layout;
+type MenuItem = Required<MenuProps>['items'][number];
+
+const Routers: (RouteProps & MenuItem)[] = [
   {
     path: '/',
+    key: '/',
+    label: 'mock',
+    icon: <AuditOutlined />,
     element: <Index />,
+  },
+  {
+    path: '/generatedCode',
+    key: '/generatedCode',
+    label: '生成代码',
+    icon: <CodeOutlined />,
+    element: <GeneratedCode />,
   },
 ];
 
 const RouterView = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
-    <BrowserRouter>
-      <div className="router-pages">
-        <Routes>
-          {Routers.map((item) => {
-            return <Route key={item.path} {...item} />;
-          })}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Layout style={{ height: '100%' }}>
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={collapsed}
+          width={200}
+          style={{ background: colorBgContainer }}
+        >
+          <div className="layout-sider-title">
+            <div className={cls('title-text', { collapsed })}>
+              <div className="text-show-text">可视化管理</div>
+            </div>
+            <Button
+              onClick={() => setCollapsed(!collapsed)}
+              className={cls('collpased-btn', { collapsed })}
+              icon={<MenuFoldOutlined />}
+              type="text"
+            />
+          </div>
+          <Menu
+            mode="inline"
+            selectedKeys={[location.pathname]}
+            onClick={({ key }) => location.pathname !== key && navigate(key)}
+            defaultSelectedKeys={['/']}
+            defaultOpenKeys={['sub1']}
+            items={Routers}
+          />
+        </Sider>
+        <Content
+          style={{
+            height: '100%',
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <div className="router-pages">
+            <Routes>
+              {Routers.map((item) => {
+                return (
+                  <Route
+                    path={item.path}
+                    element={item.element}
+                    key={item.key}
+                  />
+                );
+              })}
+            </Routes>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 export default RouterView;
