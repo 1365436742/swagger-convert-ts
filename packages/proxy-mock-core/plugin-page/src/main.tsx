8ba2a4ab1bcf4dc349707c29a3913551f1bf1519
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:22:46 +0800

    Update main.tsx

--- packages/proxy-mock-core/plugin-page/src/main.tsx
+++ packages/proxy-mock-core/plugin-page/src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import RouterView from './routers';
 import zhCN from 'antd/locale/zh_CN';
 import { ConfigProvider } from 'antd';
+import './index.less';
 createRoot(document.getElementById('root')!).render(
-  <ConfigProvider locale={zhCN}>
-    <RouterView />
-  </ConfigProvider>
+  <BrowserRouter>
+    <ConfigProvider locale={zhCN}>
+      <RouterView />
+    </ConfigProvider>
+  </BrowserRouter>
 );
