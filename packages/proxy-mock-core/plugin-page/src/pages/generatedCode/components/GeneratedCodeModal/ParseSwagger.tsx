aa5f78cbdee7a6d6e699c538c76d85609a86520d
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:54:32 +0800

    Create ParseSwagger.tsx

---
+++ packages/proxy-mock-core/plugin-page/src/pages/generatedCode/components/GeneratedCodeModal/ParseSwagger.tsx
@@ -0,0 +1,33 @@
+import { Button, Flex, Input, Popover } from 'antd';
+import React from 'react';
+import FormItemMoncaoEditor from '../../../../components/FormItemMoncaoEditor';
+interface ParseSwaggerProps {
+  value?: string;
+  onChange?: () => void;
+}
+const ParseSwagger: React.FC<ParseSwaggerProps> = ({ value, onChange }) => {
+  return (
+    <Flex gap="small">
+      <Input
+        value={value}
+        onChange={onChange}
+        placeholder="例如：http://localhost:8081/v3/api-docs"
+      />
+      <Popover
+        trigger="click"
+        content={
+          <div style={{ width: '600px' }}>
+            <FormItemMoncaoEditor
+              editorProps={{ height: '366px', defaultLanguage: 'json' }}
+            />
+          </div>
+        }
+      >
+        <Button>json内容</Button>
+      </Popover>
+      <Button>解析</Button>
+    </Flex>
+  );
+};
+
+export default ParseSwagger;
