4b47146943e77ea60ba9d0a5cf106f74615b3062
1365436742 <51362520+1365436742@users.noreply.github.com>
2024-12-24 16:38:30 +0800

    Create index.tsx

---
+++ packages/proxy-mock-core/plugin-page/src/pages/generatedCode/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.less';
+import Header from '../../components/Header';
+import SearchArea from './components/SearchArea';
+import ListItem from './components/ListItem';
+const GeneratedCode: React.FC = () => {
+  return (
+    <div className="layout-page">
+      <Header>
+        <SearchArea></SearchArea>
+      </Header>
+      <ListItem></ListItem>
+      <ListItem></ListItem>
+      <ListItem></ListItem>
+      <ListItem></ListItem>
+    </div>
+  );
+};
+
+export default GeneratedCode;
