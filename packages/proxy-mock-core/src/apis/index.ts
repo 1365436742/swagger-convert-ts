5ac5caa2c5ed36058fe9c92472d16374c2f3308f
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-02 17:08:24 +0800

    Update index.ts

--- packages/proxy-mock-core/src/apis/index.ts
+++ packages/proxy-mock-core/src/apis/index.ts
@@ -1,8 +1,15 @@
+import { message } from 'antd';
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.PROD ? '' : 'http://localhost:3001/api',
+  baseURL: import.meta.env.PROD ? '' : 'http://localhost:3001',
   timeout: 1000,
+});
+instance.interceptors.response.use((response) => {
+  if (response.data.status === 0) {
+    message.error(response.data.message);
+  }
+  return response;
 });
 
 export interface ResResult<T = any> {
