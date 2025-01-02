33e77b1f1676744c6defbce45aaed7a27e3e2f4a
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-02 16:26:44 +0800

    Update index.tsx

--- packages/proxy-mock-core/src/pages/index/components/ListItem/index.tsx
+++ packages/proxy-mock-core/src/pages/index/components/ListItem/index.tsx
@@ -1,73 +1,164 @@
-import { Button, Flex, Popconfirm, Switch, Tag } from 'antd';
+import { Button, Flex, message, Popconfirm, Switch, Tag } from 'antd';
 import { EditOutlined, PlusOutlined } from '@ant-design/icons';
 import UpdataMockModal from '../UpdataMockModal';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import UpdataSceneModal from '../UpdataSceneModal';
+import {
+  AddMockParams,
+  MockListRes,
+  toggleMock,
+  updateMock,
+} from '../../../../apis/mock';
 import './index.less';
+import {
+  addSence,
+  selectSence,
+  senceDetail,
+  updateSence,
+  UpdateSenceParams,
+} from '../../../../apis/mockSence';
 
-const ListItem = () => {
+interface ListItemProps {
+  item: MockListRes;
+  onUpdateList?: () => void;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ item, onUpdateList }) => {
+  const { url, method } = item;
   const [opneUpdataMockModal, setOpneUpdataMockModal] = useState(false);
   const [opneUpdataSceneModal, setOpneUpdataSceneModal] = useState(false);
+
   const [opneEditUpdataSceneModal, setEditOpneUpdataSceneModal] =
     useState(false);
+  const [initialValuesMock, setInitialValuesMock] = useState<AddMockParams>();
+  const [initialValuesSence, setInitialValuesSence] =
+    useState<UpdateSenceParams>();
+
+  const onChangeSence = async (curSence: string) => {
+    await selectSence({
+      url,
+      method,
+      senceName: curSence,
+    });
+  };
+
+  const onChangeSenceContent = (curSence: string) => {
+    senceDetail({ url, method, senceName: curSence }).then((res) => {
+      if (res.data.status === 1) {
+        setInitialValuesSence({
+          url,
+          method,
+          senceName: curSence,
+          senceContent: res.data.data.senceContent,
+          oldSenceName: curSence,
+        });
+        setEditOpneUpdataSceneModal(true);
+      }
+    });
+  };
 
   return (
     <div className="list-item">
       <UpdataMockModal
+        initialValues={initialValuesMock}
         title="修改mock数据配置"
         btnText="修改"
         open={opneUpdataMockModal}
         onChange={setOpneUpdataMockModal}
+        onFinish={(values) => {
+          updateMock({
+            ...values,
+            sourceFileInfo: {
+              url,
+              method,
+            },
+          })
+            .then((res) => {
+              if (res.data.status === 1) {
+                message.success(res.data.message);
+                onUpdateList?.();
+              }
+            })
+            .finally(() => {
+              setOpneUpdataMockModal(false);
+            });
+        }}
       ></UpdataMockModal>
       <UpdataSceneModal
+        onFinish={(values) => {
+          const { senceName, senceContent } = values;
+          addSence({ url, method, senceName, senceContent })
+            .then((res) => {
+              if (res.data.status === 1) {
+                message.success(res.data.message);
+                onUpdateList?.();
+              }
+            })
+            .finally(() => {
+              setOpneUpdataSceneModal(false);
+            });
+        }}
         open={opneUpdataSceneModal}
         onChange={setOpneUpdataSceneModal}
       ></UpdataSceneModal>
       <UpdataSceneModal
+        initialValues={initialValuesSence}
         title="修改场景数据"
         btnText="修改"
         open={opneEditUpdataSceneModal}
         onChange={setEditOpneUpdataSceneModal}
+        onFinish={(values) => {
+          updateSence({
+            url,
+            method,
+            senceContent: values.senceContent,
+            senceName: values.senceName,
+            oldSenceName: values.oldSenceName,
+          });
+        }}
       ></UpdataSceneModal>
       <div className="left">
         <Flex gap="small" className="action">
-          <Switch checkedChildren="开启" unCheckedChildren="关闭" />
-          <Tag color="green">default</Tag>
-          <Tag color="red">延时：0s</Tag>
+          <Switch
+            value={item.mock}
+            checkedChildren="开启"
+            unCheckedChildren="关闭"
+            onChange={() => toggleMock({ url, method })}
+          />
+          <Tag color="green">{item.sence}</Tag>
+          {item.delay && <Tag color="red">延时：{item.delay}ms</Tag>}
           <Tag bordered={false} color="processing">
-            GET
+            {method}
           </Tag>
-          <div className="title">/api/apiUrl</div>
+          <div className="title">{url}</div>
         </Flex>
         <Flex gap="small" className="radion-content" wrap>
-          <Button
-            icon={
-              <EditOutlined
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setEditOpneUpdataSceneModal(true);
-                }}
-              />
+          {item.senceList.map((sence) => {
+            let selectProps = {};
+            if (sence === item.sence) {
+              selectProps = {
+                color: 'primary',
+                variant: 'outlined',
+              };
             }
-            iconPosition="end"
-          >
-            场景1
-          </Button>
-          <Button
-            color="primary"
-            variant="outlined"
-            icon={
-              <EditOutlined
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setEditOpneUpdataSceneModal(true);
-                }}
-              />
-            }
-            iconPosition="end"
-          >
-            场景1
-          </Button>
+            return (
+              <Button
+                {...selectProps}
+                icon={
+                  <EditOutlined
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onChangeSenceContent(sence);
+                    }}
+                  />
+                }
+                iconPosition="end"
+                onClick={() => onChangeSence(sence)}
+              >
+                {sence}
+              </Button>
+            );
+          })}
           <Button
             type="dashed"
             icon={<PlusOutlined />}
@@ -79,7 +170,19 @@ const ListItem = () => {
       </div>
       <div className="btnAction">
         <Flex gap="small">
-          <Button type="primary" onClick={() => setOpneUpdataMockModal(true)}>
+          <Button
+            type="primary"
+            onClick={() => {
+              setInitialValuesMock({
+                url,
+                method,
+                sence: item.sence,
+                mock: item.mock,
+                delay: item.delay,
+              });
+              setOpneUpdataMockModal(true);
+            }}
+          >
             修改配置
           </Button>
           <Popconfirm
