0b1418926d28650322edfc7d59c7c14f69f7aab0
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-02 14:53:23 +0800

    Create index.ts

---
+++ packages/proxy-mock-core/src/apis/mock/index.ts
@@ -0,0 +1,51 @@
+import instance, { ResResult } from '..';
+
+export interface AddMockParams {
+  sence?: string;
+  mock: boolean;
+  delay: number;
+  url: string;
+  method: string;
+}
+
+export interface DeleteMockParams {
+  url: string;
+  method: string;
+}
+
+export interface UpdateMockParams {
+  sence?: string;
+  mock: boolean;
+  delay: number;
+  url: string;
+  method: string;
+  sourceFileInfo: {
+    url: string;
+    method: string;
+  };
+}
+
+export interface MockListRes {
+  sence?: string;
+  mock: boolean;
+  delay: number;
+  url: string;
+  method: string;
+  senceList: string[];
+}
+
+export const addMock = (params: AddMockParams) => {
+  return instance.post<ResResult>('/mock/addMock', params);
+};
+
+export const deleteMock = (params: AddMockParams) => {
+  return instance.post<ResResult>('/mock/deleteMock', params);
+};
+
+export const updateMock = (params: UpdateMockParams) => {
+  return instance.post<ResResult>('/mock/updateMock', params);
+};
+
+export const mockList = () => {
+  return instance.get<ResResult<MockListRes>>('/mock/mockList');
+};
