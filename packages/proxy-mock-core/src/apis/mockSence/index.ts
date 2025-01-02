786d8a5d94689cc056404da7734265728e8bb35a
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-02 14:53:50 +0800

    Create index.ts

---
+++ packages/proxy-mock-core/src/apis/mockSence/index.ts
@@ -0,0 +1,44 @@
+import instance, { ResResult } from '..';
+
+export interface AddSenceParams {
+  url: string;
+  method: string;
+  senceName: string;
+  senceContent: string;
+}
+
+export interface DeleteSenceParams {
+  url: string;
+  method: string;
+  senceName: string;
+}
+
+export interface UpdateSenceParams {
+  url: string;
+  method: string;
+  senceName: string;
+  senceContent: string;
+}
+
+export interface SenceDetailRes {
+  senceName: string;
+  senceContent: string;
+}
+export const addSence = (params: AddSenceParams) => {
+  return instance.post<ResResult>('/mockSence/addSence', params);
+};
+
+export const deleteSence = (params: DeleteSenceParams) => {
+  return instance.post<ResResult>('/mockSence/deleteSence', params);
+};
+
+export const updateSence = (params: UpdateSenceParams) => {
+  return instance.post<ResResult>('/mockSence/updateSence', params);
+};
+
+export const senceDetail = (params: DeleteSenceParams) => {
+  return instance.post<ResResult<SenceDetailRes>>(
+    '/mockSence/senceDetail',
+    params
+  );
+};
