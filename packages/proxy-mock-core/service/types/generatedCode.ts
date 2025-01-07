8f63f12043f7fec1ca5bb5b7e84822baeafde202
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-07 15:46:21 +0800

    Update generatedCode.ts

--- packages/proxy-mock-core/service/types/generatedCode.ts
+++ packages/proxy-mock-core/service/types/generatedCode.ts
@@ -1,11 +1,18 @@
 export interface GeneratedCodeRequestList {
-    key?: string
-    requestUrl: string
-    method: string
-    responseMockjs?: Record<string, any>
+  key?: string;
+  requestUrl: string;
+  method: string;
+  responseMockjs?: Record<string, any>;
 }
 
 export interface MockJsParams {
-    swaggerUrl: string,
-    generatedCodeList: GeneratedCodeRequestList[]
+  swaggerUrl: string;
+  generatedCodeList: GeneratedCodeRequestList[];
+}
+export interface SpaceConfigJson{
+  filterUrl: string;
+}
+export interface SapceItem {
+  spaceName: string;
+  configJson: SpaceConfigJson;
 }
