df7ca2e383213ca5fd4c01da1c1cd64f9064ebf2
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-23 17:12:36 +0800

    Update vite-dev.service.ts

--- packages/proxy-mock-core/vite-dev.service.ts
+++ packages/proxy-mock-core/vite-dev.service.ts
@@ -1,29 +1,38 @@
-import { Plugin } from 'vite'
+import { ConfigEnv, Plugin, UserConfig } from 'vite'
 import mainService from './dist/index'
 import path from 'path'
 import { MainServiceReturn } from './dist/types'
 const generatedCodeFileUrl = path.resolve(__dirname, './eg-test/request-apis')
 const mockDataFileUrl = path.resolve(__dirname, './eg-test/mock')
 let mainServiceInfo: MainServiceReturn | null = null
-export default function devServicePlugin(): Promise<Plugin<any>> {
-  return new Promise<Plugin<any>>(async resolve => {
-    if (!mainServiceInfo) {
-      mainServiceInfo = await mainService({
-        port: 3001,
-        generatedCodeFileUrl,
-        mockDataFileUrl,
-      })
-    }
-    mainServiceInfo.getMockInfo('app/list', 'get').then((res: any) => {
-      console.log(res, 'xxxxx')
-    })
-    resolve({
-      name: 'dev-service-plugin', // 插件名称
-      handleHotUpdate() {
-        setTimeout(() => {
-          console.log('服务端口', mainServiceInfo?.serviceUrl)
-        }, 0)
-      },
-    })
-  })
+export default function devServicePlugin(): Plugin {
+  return {
+    name: 'dev-service-plugin',
+    // 配置
+    async config(conf: UserConfig, env: ConfigEnv) {
+      if (env.command === 'serve') {
+        console.log(mainServiceInfo, 'xxxxx')
+        if (!mainServiceInfo) {
+          mainServiceInfo = await mainService({
+            port: 3001,
+            generatedCodeFileUrl,
+            mockDataFileUrl,
+          })
+        }
+        mainServiceInfo.getMockInfo('app/list', 'get').then((res: any) => {
+          console.log(res, 'xxxxx')
+        })
+      }
+      return conf
+    },
+    handleHotUpdate(ctx) {
+      ctx.server.config.logger.info(
+        `\x1b[32m\mock访问地址: \x1b[0m \x1b[34m${mainServiceInfo?.serviceUrl}\x1b[0m`,
+        {
+          clear: false,
+          timestamp: true,
+        },
+      )
+    },
+  }
 }
