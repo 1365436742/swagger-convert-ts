8ae0f6fe74e52ddf364066bde01b472273e9ec89
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-23 16:11:51 +0800

    Update vite-dev.service.ts

--- packages/proxy-mock-core/vite-dev.service.ts
+++ packages/proxy-mock-core/vite-dev.service.ts
@@ -1,22 +1,29 @@
-import { PluginOption } from 'vite'
+import { Plugin } from 'vite'
 import mainService from './dist/index'
 import path from 'path'
+import { MainServiceReturn } from './dist/types'
 const generatedCodeFileUrl = path.resolve(__dirname, './eg-test/request-apis')
 const mockDataFileUrl = path.resolve(__dirname, './eg-test/mock')
-const mainServiceInfo = mainService({
-  generatedCodeFileUrl,
-  mockDataFileUrl,
-})
-export default function devServicePlugin(): PluginOption {
-  mainServiceInfo.getMockInfo('app/list', 'get').then((res: any) => {
-    console.log(res, 'xxxxx')
+let mainServiceInfo: MainServiceReturn | null = null
+export default function devServicePlugin(): Promise<Plugin<any>> {
+  return new Promise<Plugin<any>>(async resolve => {
+    if (!mainServiceInfo) {
+      mainServiceInfo = await mainService({
+        port: 3001,
+        generatedCodeFileUrl,
+        mockDataFileUrl,
+      })
+    }
+    mainServiceInfo.getMockInfo('app/list', 'get').then((res: any) => {
+      console.log(res, 'xxxxx')
+    })
+    resolve({
+      name: 'dev-service-plugin', // 插件名称
+      handleHotUpdate() {
+        setTimeout(() => {
+          console.log('服务端口', mainServiceInfo?.serviceUrl)
+        }, 0)
+      },
+    })
   })
-  return {
-    name: 'dev-service-plugin', // 插件名称
-    handleHotUpdate() {
-      setTimeout(() => {
-        console.log('服务端口', mainServiceInfo.serviceUrl)
-      }, 0)
-    },
-  }
 }
