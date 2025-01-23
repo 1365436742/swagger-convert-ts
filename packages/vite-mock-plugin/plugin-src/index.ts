9ea05e46a8268471dc9a95bc9fc2909a99175145
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-01-23 17:01:06 +0800

    Update index.ts

--- packages/vite-mock-plugin/plugin-src/index.ts
+++ packages/vite-mock-plugin/plugin-src/index.ts
@@ -1,5 +1,5 @@
 // vite-plugin-full.ts
-import type { Plugin, UserConfig, ConfigEnv, ResolvedConfig } from 'vite'
+import type { Plugin, UserConfig, ConfigEnv } from 'vite'
 import mainService from 'proxy-mock-core'
 import {
   ConfigOptions,
@@ -13,78 +13,74 @@ const DefaultOption: ConfigOptions = {
   mockDataFileUrl: path.join(__dirname, 'mock'),
 }
 let mainServiceInfo: MainServiceReturn | null = null
-export default function ProxyMockPlugin(
-  options: ConfigOptions = {},
-): Promise<Plugin> {
-  return new Promise<Plugin<any>>(async resolve => {
-    resolve({
-      name: 'vite-plugin-proxy-mock',
-      // 配置
-      async config(conf: UserConfig, env: ConfigEnv) {
-        if (conf.server?.proxy && env.command === 'serve') {
-          if (!mainServiceInfo) {
-            mainServiceInfo = await mainService(
-              Object.assign(DefaultOption, options),
-            )
-          }
-          for (const key in conf.server?.proxy) {
-            const proxItem = conf.server?.proxy[key]
-            if (typeof proxItem === 'object') {
-              const oldConfigure = proxItem.configure
-              conf.server.proxy[key] = {
-                ...proxItem,
-                selfHandleResponse: true,
-                configure(...params) {
-                  oldConfigure?.(...params)
-                  const [proxy] = params
-                  proxy.on(
-                    'proxyRes',
-                    (proxyRes, req: IncomingMessage & any, res) => {
-                      var body: any[] = []
-                      proxyRes.on('data', function (chunk: any) {
-                        body.push(chunk)
-                      })
-                      proxyRes.on('end', async () => {
-                        if (!mainServiceInfo) {
-                          res.end(Buffer.concat(body).toString())
-                          return
-                        }
-                        const url = req.originalUrl || req.url || ''
-                        const pathname = url.split('?')[0]
-                        const json = await mainServiceInfo.getMockInfo(
-                          pathname,
-                          req.method || '',
-                          { proxyRes, req, res },
-                        )
-                        if (!json) {
-                          res.end(Buffer.concat(body).toString())
-                        } else {
-                          res.setHeader('Content-Type', 'application/json')
-                          res.writeHead(200)
-                          res.end(JSON.stringify(json))
-                        }
-                      })
-                    },
-                  )
-                },
-              }
+export default function ProxyMockPlugin(options: ConfigOptions = {}): Plugin {
+  return {
+    name: 'vite-plugin-proxy-mock',
+    // 配置
+    async config(conf: UserConfig, env: ConfigEnv) {
+      if (conf.server?.proxy && env.command === 'serve') {
+        if (!mainServiceInfo) {
+          mainServiceInfo = await mainService(
+            Object.assign(DefaultOption, options),
+          )
+        }
+        for (const key in conf.server?.proxy) {
+          const proxItem = conf.server?.proxy[key]
+          if (typeof proxItem === 'object') {
+            const oldConfigure = proxItem.configure
+            conf.server.proxy[key] = {
+              ...proxItem,
+              selfHandleResponse: true,
+              configure(...params) {
+                oldConfigure?.(...params)
+                const [proxy] = params
+                proxy.on(
+                  'proxyRes',
+                  (proxyRes, req: IncomingMessage & any, res) => {
+                    var body: any[] = []
+                    proxyRes.on('data', function (chunk: any) {
+                      body.push(chunk)
+                    })
+                    proxyRes.on('end', async () => {
+                      if (!mainServiceInfo) {
+                        res.end(Buffer.concat(body).toString())
+                        return
+                      }
+                      const url = req.originalUrl || req.url || ''
+                      const pathname = url.split('?')[0]
+                      const json = await mainServiceInfo.getMockInfo(
+                        pathname,
+                        req.method || '',
+                        { proxyRes, req, res },
+                      )
+                      if (!json) {
+                        res.end(Buffer.concat(body).toString())
+                      } else {
+                        res.setHeader('Content-Type', 'application/json')
+                        res.writeHead(200)
+                        res.end(JSON.stringify(json))
+                      }
+                    })
+                  },
+                )
+              },
             }
           }
         }
-        return {
-          ...conf,
-          // 添加配置
-        }
-      },
-      handleHotUpdate(ctx) {
-        ctx.server.config.logger.info(
-          `\x1b[32m\mock访问地址: \x1b[0m \x1b[34m${mainServiceInfo?.serviceUrl}\x1b[0m`,
-          {
-            clear: false,
-            timestamp: true,
-          },
-        )
-      },
-    })
-  })
+      }
+      return {
+        ...conf,
+        // 添加配置
+      }
+    },
+    handleHotUpdate(ctx) {
+      ctx.server.config.logger.info(
+        `\x1b[32m\mock访问地址: \x1b[0m \x1b[34m${mainServiceInfo?.serviceUrl}\x1b[0m`,
+        {
+          clear: false,
+          timestamp: true,
+        },
+      )
+    },
+  }
 }
