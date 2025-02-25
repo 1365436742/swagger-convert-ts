0419e77fcdc2862f95b154dcda15537a5aed76c2
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-25 14:54:05 +0800

    Update index.js

--- packages/webpack-mock-plugin/example/index.js
+++ packages/webpack-mock-plugin/example/index.js
@@ -1,1 +1,15 @@
-console.log('启动')
+import { fetchEventSource } from '@microsoft/fetch-event-source'
+fetchEventSource("/api/eventStream", {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ a: 22 }),
+  openWhenHidden: true,
+  onmessage(msg) {
+    console.log('msg=>>>', msg)
+  },
+  onerror(err) {
+    console.log('err=>>>', err)
+  },
+})
