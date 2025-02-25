0e47f1f5a5bd37945ef2c0a25dd5adaa944e3f62
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-25 15:44:00 +0800

    Update main.ts

--- packages/vite-mock-plugin/src/main.ts
+++ packages/vite-mock-plugin/src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import { fetchEventSource } from '@microsoft/fetch-event-source'
+fetchEventSource('/api/eventStream', {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ a: 22 }),
+  openWhenHidden: true,
+  onmessage(msg) {
+    console.log('msg=>>>', msg)
+  },
+  onerror(err) {
+    console.log('err=>>>', err)
+  },
+})
 createApp(App).mount('#app')
