0a36ca81161a57e35a55e4fca894fbd9b1f486e5
1365436742 <51362520+1365436742@users.noreply.github.com>
2025-02-25 14:55:01 +0800

    Create default.js

---
+++ packages/webpack-mock-plugin/mockInfo/mock/api_eventStream_POST/default.js
@@ -0,0 +1,28 @@
+module.exports = async ({ req, res }) => {
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.statusCode = 200;
+  let timeoutId;
+  // 页面刷新要进行关闭
+  res.on('close', () => {
+    timeoutId && clearInterval(timeoutId);
+    if (!res.writableEnded) {
+      res.end();
+    }
+  });
+  // 自定义 JSON 内容
+  const customData = [
+    { id: 1, message: 'First chunk' },
+    { id: 2, message: 'Second chunk' },
+    { id: 3, message: 'Third chunk' },
+  ]
+  // 分段发送自定义 JSON 内容
+  for (const data of customData) {
+    res.write(`data: ${JSON.stringify(data)}\n\n`)
+    await new Promise(resolve => {
+      // 定时时间间隔
+      timeoutId = setTimeout(resolve, 1000)
+    }) // 模拟延迟
+  }
+  res.end()
+  return {}
+}
